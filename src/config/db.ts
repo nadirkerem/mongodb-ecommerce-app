c8d73adaeca07a1c5de807875b235c4bd3cc6d03
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-07-23 11:35:17 -0400

    initial commit

---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default async function connectDB() {
+  const MONGO_URI = process.env.MONGO_URI;
+
+  const options = {
+    dbName: 'sample_restaurants',
+  };
+
+  try {
+    if (!MONGO_URI) {
+      throw new Error('MONGO_URI is missing in env');
+    }
+
+    await mongoose.connect(MONGO_URI, options);
+  } catch (error) {
+    console.error('Error connecting to database: ', error);
+    process.exit(1);
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+
+import connectDB from './config/db';
+import errorHandler from './middlewares/error-handler';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app: Application = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, world!');
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on: http://localhost:${PORT}`);
+});
